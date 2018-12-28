8aced2d0356e4f436ee8f5cb9d420cb4673950d9
HelmiHalmouch <helmirealmadrid@gmail.com>
2018-12-28 21:28:33 +0100

    add zip command in same folder

--- unzip_dataset_file.py
+++ unzip_dataset_file.py
@@ -2,9 +2,9 @@
 import zipfile
 import sys, os 
 
-file = input('Please enter the input file name as file_name.zip',)
+file = input('Please enter the input file name as file_name.zip:',)
 
 with zipfile.ZipFile(file, 'r') as zip_ref:
-    zip_ref.extractall(os.path.splitext(file)[0])
+    zip_ref.extractall()
 
-print('Processing finished !!!')
+print('Unzip finished !!!')
